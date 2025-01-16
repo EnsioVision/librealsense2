e338f507409841cf015ae869309ece85326bc937
Jon McGowan <jon.mcgowan@ensiovision.com>
2025-01-16 14:46:18 +0000

    Install udev rules script

---
+++ install_udev_rules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install udev-rules
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
+sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger 
+
